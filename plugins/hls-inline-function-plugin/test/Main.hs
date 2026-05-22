b8b47e0ed63daa1c14e91b394ec95101494f7619
Simon Horlick <simonhorlick@gmail.com>
2026-05-22 13:49:31 +0800

    Add test case for where binding

--- plugins/hls-inline-function-plugin/test/Main.hs
+++ plugins/hls-inline-function-plugin/test/Main.hs
@@ -122,6 +122,7 @@ resolveTests = testGroup "resolve" [
   , runTest "Offers inlining for type class methods" "Inline identity" "Class" (Position 6 13)
   , runTest "Offers inlining for let bindings" "Inline y" "Let2" (Position 5 5)
   , runTest "Offers inlining for bindings in where clauses" "Inline foo" "Where" (Position 2 11)
+  , runTest "Offers inlining for operators" "Inline /*" "Operator" (Position 4 15)
   ]
 
 actionTests :: TestTree
