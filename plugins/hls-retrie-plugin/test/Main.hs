91b8101eca7e0eab71db2c2d9239660cdd3579af
Simon Horlick <simonhorlick@gmail.com>
2026-08-30 11:39:56 +0800

    Add expected-fail test: visible type application at the site is not matched

--- plugins/hls-retrie-plugin/test/Main.hs
+++ plugins/hls-retrie-plugin/test/Main.hs
@@ -96,6 +96,8 @@ inlineThisTests =
           testCommand "signature-fixed type is kept at the site" "SigConstraint" 7 9
       , expectFailBecause "a body annotation naming a signature type variable is spliced verbatim" $
           testCommand "refuses a body that names a scoped type variable" "ScopedTyVar" 9 4
+      , expectFailBecause "a visible type application at the site is not matched" $
+          testCommand "visible type application at the site" "TypeAppSite" 7 4
       ]
     , testGroup "imports"
       [ testCommand "appends an import" "CrossFileUse" 6 4
---
+++ plugins/hls-retrie-plugin/test/testdata/TypeAppSite.expected.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE TypeApplications #-}
+module TypeAppSite where
+
+e :: Num a => a -> a
+e x = x + 1
+
+f :: Int
+f = 6 + 1
